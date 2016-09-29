942bc86336b495181592c60cb6054680d5327bb6
developerdong <developerdong@qq.com>
2016-09-29 16:49:50 +0800

    注册函数中render()->renderText()

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -16,11 +16,10 @@ public class UserController extends Controller {
 			User user = new User();
 			user.set("username", getPara("username")).set("password", getPara("password")).save();
 			new StorageHandler("hdfs://localhost:9000", user.getInt("id")).createUserRoot();
-
-			render("注册成功", 200);
+            renderText("注册成功");
 		}
 		catch(Exception e){
-			render("注册失败", 503);
+			renderText("用户名已存在");
 		}
 	}
 }
