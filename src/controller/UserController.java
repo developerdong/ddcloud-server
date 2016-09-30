5f9f249bca12a46f34da886511f1abc157679eb1
developerdong <developerdong@qq.com>
2016-09-30 09:11:46 +0800

    完成login函数

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -4,9 +4,28 @@ import com.jfinal.core.Controller;
 import handler.StorageHandler;
 import model.User;
 
+import java.security.MessageDigest;
+import java.util.Date;
+import java.util.HashMap;
+
+
 public class UserController extends Controller {
-	public void login(){
-		
+	private static HashMap<String, Integer> tokenMap = new HashMap<>();
+	public void login() throws Exception{
+		User user = User.dao.findFirst("select * from user where username='" + getPara("username") + "'");
+		if(user.getStr("password").equals(getPara("password"))){
+            String token = new HashGeneratorUtils().hashString(getPara("username") + new Date().getTime(), "MD5");
+			tokenMap.put(token, user.getInt("id"));
+
+            setAttr("status", 200);
+            setAttr("token", token);
+            renderJson();
+		}
+		else{
+            setAttr("status", 403);
+            setAttr("error", "用户名或密码错误");
+            renderJson();
+		}
 	}
 	public void logout(){
 
@@ -20,6 +39,30 @@ public class UserController extends Controller {
 		}
 		catch(Exception e){
 			renderText("用户名已存在");
+		}
+	}
+
+	public class HashGeneratorUtils {
+		private HashGeneratorUtils() {
+
+		}
+
+		public String hashString(String message, String algorithm)
+				throws Exception {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            byte[] hashedBytes = digest.digest(message.getBytes("UTF-8"));
+
+            return convertByteArrayToHexString(hashedBytes);
+
+		}
+
+		private String convertByteArrayToHexString(byte[] arrayBytes) {
+			StringBuffer stringBuffer = new StringBuffer();
+			for (int i = 0; i < arrayBytes.length; i++) {
+				stringBuffer.append(Integer.toString((arrayBytes[i] & 0xff) + 0x100, 16)
+						.substring(1));
+			}
+			return stringBuffer.toString();
 		}
 	}
 }
