d65cc802ffed38b8430ffd8d69f3079a296cce4f
developerdong <developerdong@qq.com>
2016-10-01 16:29:31 +0800

    完成文件上传函数

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -5,6 +5,9 @@ import com.jfinal.core.Controller;
 import handler.StorageHandler;
 import interceptor.tokenInterceptor;
 
+import java.io.File;
+import java.io.IOException;
+
 @Before(tokenInterceptor.class)
 public class FileController extends Controller {
     private StorageHandler storage;
@@ -13,13 +16,31 @@ public class FileController extends Controller {
         this.storage = storage;
     }
 
-    public void upload(){
+    public void upload() {
+        File file = getFile("file").getFile();
+        String destDirPath = getPara("destDirPath");
+        try {
+            if (storage.exists(destDirPath)) {
+                storage.createFile(destDirPath, file);
+                setAttr("status", 200);
+                setAttr("result", "创建成功");
+            } else {
+                setAttr("status", 403);
+                setAttr("error", "目录不存在");
+            }
+        } catch (IOException e) {
+            setAttr("status", 500);
+            setAttr("error", "创建失败");
+        } finally {
+            renderJson();
+        }
+    }
 
-	}
-	public void download(){
-		
-	}
-	public void delete(){
-		
-	}
+    public void download() {
+
+    }
+
+    public void delete() {
+
+    }
 }
